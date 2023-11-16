037d9eb3d3abf3c21e419ec86b99799d5924e504
noamanmazhar <noamanmazhar@gmail.com>
2023-11-16 13:03:35 +0300

    Revert "water"
    This reverts commit ed14fb6d9c4a8494cf9085d05d8528975b865092.

--- Assets/BluetoothAPI/Scripts/manager.cs
+++ Assets/BluetoothAPI/Scripts/manager.cs
@@ -18,7 +18,7 @@ public class manager : MonoBehaviour {
 
 	public GameObject sphere;
 
-	public string received_message;
+	string received_message;
 
 	void Start () {
 		deviceName = "ESP32-BT"; //bluetooth should be turned ON;
